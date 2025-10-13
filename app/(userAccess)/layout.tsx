05bda2000548923d0240442e52b6bd3baee57c4a
MrHyaena <dolezalmartin131@gmail.com>
2025-10-13 13:59:36 +0200

    login page

--- app/(userAccess)/layout.tsx
+++ app/(userAccess)/layout.tsx
@@ -12,58 +12,58 @@ import Link from "next/link";
 export default function layout({ children }: PropsWithChildren) {
   return (
     <div className="relative grid grid-cols-2 w-full h-screen bg-linear-140 from-qart to-70% to-primary   p-20">
-      <div className="col-span-2 flex items-center gap-10">
-        <Image
-          src={logoWhite}
-          width={500}
-          height={500}
-          alt="logo"
-          className="max-h-15 h-full w-auto"
-        />
-        <ul className="text-white font-semibold flex gap-10 italic">
-          <li>
-            <Link href={"/"}>
-              <Text
-                text="Katalog"
-                color="white"
-                level="label3"
-                className="hover:scale-105 animate"
-              />
-            </Link>
-          </li>
-          <li>
-            <Link href={"/"}>
-              <Text
-                text="FAQ"
-                color="white"
-                level="label3"
-                className="hover:scale-105 animate"
-              />
-            </Link>
-          </li>
-          <li>
-            <Link href={"/"}>
-              <Text
-                text="Jak to funguje"
-                color="white"
-                level="label3"
-                className="hover:scale-105 animate"
-              />
-            </Link>
-          </li>
-          <li>
-            <Link href={"/"}>
-              <Text
-                text="Hlavní stránka"
-                color="white"
-                level="label3"
-                className="hover:scale-105 animate"
-              />
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="flex flex-col items-start justify-end">
+      <div className="flex flex-col items-start justify-between">
+        <div className="col-span-2 flex items-center gap-10">
+          <Image
+            src={logoWhite}
+            width={500}
+            height={500}
+            alt="logo"
+            className="max-h-15 h-full w-auto"
+          />
+          <ul className="text-white font-semibold flex gap-10 italic">
+            <li>
+              <Link href={"/"}>
+                <Text
+                  text="Katalog"
+                  color="white"
+                  level="label3"
+                  className="hover:scale-105 animate"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link href={"/"}>
+                <Text
+                  text="FAQ"
+                  color="white"
+                  level="label3"
+                  className="hover:scale-105 animate"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link href={"/"}>
+                <Text
+                  text="Jak to funguje"
+                  color="white"
+                  level="label3"
+                  className="hover:scale-105 animate"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link href={"/"}>
+                <Text
+                  text="Hlavní stránka"
+                  color="white"
+                  level="label3"
+                  className="hover:scale-105 animate"
+                />
+              </Link>
+            </li>
+          </ul>
+        </div>
         <Text
           className="italic"
           level="h3"
